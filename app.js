b4519a23f7c542478d69449e6d51609571ce72ee
KaranSJ22 <karanjamkhandi@gmail.com>
2026-02-18 10:30:19 +0530

    Fix: app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const apiKey=ProcessingInstruction.env.WEATHER_API_KEY;
+const apiKey=process.env.WEATHER_API_KEY;
 if(!apiKey){
     console.error("Critical error: No  API key Found!");
     process.exit(1);
