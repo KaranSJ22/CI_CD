7d4da13c11a25c5ef447f77e44ceffd1553ed7e0
KaranSJ22 <karanjamkhandi@gmail.com>
2026-02-18 10:23:08 +0530

    Updated workflow for javascript

---
+++ app.js
@@ -0,0 +1,7 @@
+const apiKey=ProcessingInstruction.env.WEATHER_API_KEY;
+if(!apiKey){
+    console.error("Critical error: No  API key Found!");
+    process.exit(1);
+}
+
+console.log("App is running securely.");
